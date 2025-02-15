47f547a85ec0ff997e67b69b32ca70ebed69fd5c
Rivva Chahwala <rivvaa@gmail.com>
2025-02-16 00:05:53 +0530

    Compiled bytecode

--- main.py
+++ main.py
@@ -1,8 +1,82 @@
+def compile_to_bytecode(code):
+    bytecode = []
+    loop_stack = []
+    pc = 0
+
+    while pc < len(code):
+        command = code[pc]
+
+        if command in ['>', '<', '+', '-']:
+            count = 1
+            while pc + count < len(code) and code[pc + count] == command:
+                count += 1
+            bytecode.append((command, count))
+            pc += count - 1
+        elif command == '.':
+            bytecode.append(('.',))
+        elif command == ',':
+            bytecode.append((',',))
+        elif command == '[':
+            if pc + 2 < len(code) and code[pc + 1] == '-' and code[pc + 2] == ']':
+                bytecode.append(('[-]',))
+                pc += 2
+            else:
+                bytecode.append(('[', len(bytecode)))
+                loop_stack.append(len(bytecode) - 1)
+        elif command == ']':
+            if not loop_stack:
+                raise SyntaxError("Unmatched loop end")
+            start = loop_stack.pop()
+            bytecode.append((']', start))
+            bytecode[start] = ('[', len(bytecode) - 1)
+
+        pc += 1
+
+    if loop_stack:
+        raise SyntaxError("Unmatched loop start")
+
+    return bytecode
+
+def execute_bytecode(bytecode):
+    memory = [0] * 30000
+    ptr = 0
+    pc = 0
+    output = []
+
+    while pc < len(bytecode):
+        instruction, *args = bytecode[pc]
+
+        if instruction == '>':
+            ptr += args[0]
+        elif instruction == '<':
+            ptr -= args[0]
+        elif instruction == '+':
+            memory[ptr] = (memory[ptr] + args[0]) % 256
+        elif instruction == '-':
+            memory[ptr] = (memory[ptr] - args[0]) % 256
+        elif instruction == '.':
+            output.append(chr(memory[ptr]))
+        elif instruction == ',':
+            memory[ptr] = ord(input()[0])
+        elif instruction == '[-]':
+            memory[ptr] = 0
+        elif instruction == '[':
+            if memory[ptr] == 0:
+                pc = args[0]
+        elif instruction == ']':
+            if memory[ptr] != 0:
+                pc = args[0]
+
+        pc += 1
+
+    print(''.join(output))
+
 def repl():
     while True:
         try:
             code = input("CCBF> ")
-            print(code)
+            bytecode = compile_to_bytecode(code)
+            execute_bytecode(bytecode)
         except EOFError:
             break
 
@@ -13,4 +87,5 @@ if __name__ == "__main__":
     else:
         with open(sys.argv[1], 'r') as file:
             code = file.read()
-            print(code)
+            bytecode = compile_to_bytecode(code)
+            execute_bytecode(bytecode)
